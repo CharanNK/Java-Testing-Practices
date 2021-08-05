fbd296ab48b31362c332318d00217754cd6c4776
Charan NK <charanshetty25595@gmail.com>
2021-08-05 22:17:43 +0530

    Assert throws

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -7,11 +7,27 @@ public class MathUtils {
         return a + b;
     }
 
+    public int subtract(int a,int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a,int b) {
+        return a/b;
+    }
+
     public int[] reverseArray(int[] arr) {
-        return IntStream.rangeClosed(1,arr.length).map(i->arr[arr.length-i]).toArray();
+        return IntStream.rangeClosed(1, arr.length).map(i -> arr[arr.length - i]).toArray();
     }
 
     public List<String> interableAssertion() {
-        return Arrays.asList("one","two","three");
+        return Arrays.asList("one", "two", "three");
+    }
+
+    public double computeCircleRadius(int radius) {
+        return Math.PI * Math.pow(radius, 2);
     }
 }
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,8 +1,9 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
@@ -11,36 +12,47 @@ class MathUtilsTest {
     @Test
     void addTest() {
         int expected = 2;
-        int actual = mathUtils.add(1,1);
+        int actual = mathUtils.add(1, 1);
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual, "The method adds two numbers");
     }
 
     @Test
     void reverseArrayTest() {
-        int[] arr = {1,2,3,4,5};
-        int[] expected = {5,4,3,2,1};
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] expected = {5, 4, 3, 2, 1};
 
         int[] actual = mathUtils.reverseArray(arr);
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void iterableAssertionTest() {
-        List<String> expected = Arrays.asList("one","two","three");
+        List<String> expected = Arrays.asList("one", "two", "three");
         List<String> actual = mathUtils.interableAssertion();
 
-        assertIterableEquals(expected,actual);
+        assertIterableEquals(expected, actual);
         // Can also be done by :
         // assertArrayEquals(expected.toArray(),actual.toArray());
     }
 
     @Test
     void interableAssertionTest_noMatch() {
-        List<String> expected = Arrays.asList("one","two","three","four");
+        List<String> expected = Arrays.asList("one", "two", "three", "four");
         List<String> actual = mathUtils.interableAssertion();
 
         assertFalse(expected.equals(actual), "This will pass");
+    }
+
+    @Test
+    void testComputeCircleRadius() {
+        assertEquals(Math.PI * 10 * 10, mathUtils.computeCircleRadius(10), "Returns circle area");
+    }
+
+    @Test
+    void divideTest() {
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
+                "Divide by 0 throws ArithmeticException");
     }
 }
